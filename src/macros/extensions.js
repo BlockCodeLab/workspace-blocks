f1328f1574e1f5972d5afc80b1179aaae1b42664
Yeqin Gong <gon@gyeq.in>
2024-10-23 22:51:52 +0800

    sorted extensions by name a-z

--- src/macros/extensions.js
+++ src/macros/extensions.js
@@ -5,5 +5,12 @@ import { readdirSync } from 'node:fs';
 export function readExtensions() {
   return readdirSync(resolve(import.meta.dir, '../../../../extensions'), { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
+    .sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    })
     .map((dirent) => dirent.name);
 }
